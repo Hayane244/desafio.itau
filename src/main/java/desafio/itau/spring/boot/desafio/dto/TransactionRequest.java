b7f9a506eed6287462f21a92f335d13dc4e8837c
unknown <hayanecaetano244@gmail.com>
2025-04-04 09:34:11 -0300

    Models and DTO

---
+++ src/main/java/desafio/itau/spring/boot/desafio/dto/TransactionRequest.java
@@ -0,0 +1,25 @@
+package desafio.itau.spring.boot.desafio.dto;
+
+import java.time.OffsetDateTime;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public class TransactionRequest {
+
+	//Não aceita valores nulos.
+	@NotNull
+	//Não aceita valores zero.
+	@Min(0)
+	private double valor;
+	
+	@NotNull
+	private OffsetDateTime dataHora;
+	
+	public double getValor() {
+		return valor;
+	}
+	public OffsetDateTime getDataHora() {
+		return dataHora;
+	}	
+}
